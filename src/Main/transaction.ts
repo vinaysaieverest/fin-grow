40c4b80f70cc292d9d23450e173957948e3bb263
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-04 22:54:45 +0530

    Resloves the bugs in transaction development classes

--- src/Main/transaction.ts
+++ src/Main/transaction.ts
@@ -3,28 +3,31 @@ export class TransactionService {
   amount: number;
   type: string;
   name: string;
+  budgetType:string
+  spendType:string
   // address:string="";
 
-  constructor(amount: number, type: string, name: string) {
+  constructor(amount: number, type: string, name: string,budgetType:string,spendType:string) {
     this.amount = amount;
     this.type = type;
     this.name = name;
+    this.budgetType=budgetType;
+    this.spendType=spendType
   }
 
   async makeTransaction(): Promise<boolean> {
     try {
       const response = await axios.post('http://localhost:5005/api/transaction', {
         amount: this.amount,
-        type: this.type,
-        name: this.name
+        budgetType:this.budgetType,
+        name: this.name,
+        spendType:this.spendType,
+        type: this.type
       });
       return true;
     } catch (error) {
-    //   console.error('Error pushing data to the database:', error);
+      // console.error('Error pushing data to the database:', error);
       return false;
     }
   }
 }
-
-const transaction = new TransactionService(1000,'debit','Vinay')
-transaction.makeTransaction()
