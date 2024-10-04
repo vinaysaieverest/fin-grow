e7b5dd3612b5aada39a267c4281af1ff269c3c16
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-04 22:57:55 +0530

    Resloves the bugs in savings testing

--- src/Test/saving.test.ts
+++ src/Test/saving.test.ts
@@ -1,14 +1,35 @@
-import { data } from "../../data"
-import { progres, saving } from "../main/saving"
 
-describe('This test will describe amount savings',()=>{
-    test('should return the saved amount',()=>{
-        expect(saving(1000)).toBe(data[0].savings.currentAmount+1000)
-    })
-    test('should give progress',()=>{
-        expect(progres(data[0].savings.currentAmount)).toBe(40)
-    })
-    test('should return true if we reach our goal',()=>{
-        expect(progres(10000)).toBeTruthy
-    })
-})
+import axios from 'axios';
+import { saving } from '../main/saving';
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+describe('TransactionService', () => {
+  it('should successfully make a transaction', async () => {
+    mockedAxios.post.mockResolvedValue({ data: { success: true } });
+
+    const transaction = new saving("Vinay","Cycle",10000);
+    const result = await transaction.saving();
+
+    expect(result).toBe(true);
+    expect(mockedAxios.post).toHaveBeenCalledWith('http://localhost:5005/api/saving', {
+    name:"Vinay",
+    savingName:"Cycle",
+    amount:10000
+
+    });
+  });
+
+  it('should fail the transaction when axios throws an error', async () => {
+    mockedAxios.post.mockRejectedValue(new Error('Network Error'));
+
+    const transaction = new saving("Vinay","Cycle",10000);
+    const result = await transaction.saving();
+
+    expect(result).toBe(false); 
+    expect(mockedAxios.post).toHaveBeenCalledWith('http://localhost:5005/api/saving', {
+        name:"Vinay",
+        savingName:"Cycle",
+        amount:10000
+    }); 
+  });
+});
