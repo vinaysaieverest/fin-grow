3f031d298c61ebc27c554db61af995528706717a
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-04 22:59:10 +0530

    Resloves the bugs in testing

--- src/Main/login.ts
+++ src/Main/login.ts
@@ -4,14 +4,5 @@ export const login=(username:string,password:string)=>{
     if(username=data[0].name, password=data[0].password){
         return true
     }
-    // if(username='visa',password='1234'){
-    //     return false
-    // }
-    // if(username='',password='1234'){
-    //     return false
-    // }
-    // if(username='',password=''){
-    //     return false
-    // }
     return false;
 }
