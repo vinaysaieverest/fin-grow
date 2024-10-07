72a3e4560b349e56fcb8029e03fc8723da9aa8fc
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-07 22:07:19 +0530

    Implements the routes for register page

---
+++ routes/post/register.ts
@@ -0,0 +1,24 @@
+import { Request, Response, Router } from 'express';
+import { DataModel } from '../../models';
+import { Register } from '../../src/main/post/register';
+const express = require("express")
+const router = express.Router();
+router.post('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { name,password,salary} = req.body as { name:string,password:string,salary:number};
+        const user = await DataModel.findOne({ name });
+      if (user) {
+        res.status(409).json({ message: 'Already user exist, Please login' });
+        console.log("user exist")
+        return;
+      }
+      const user1 = new Register(name, password, salary);
+        const result = await user1.setRegister();
+        res.status(200).json({message:"User created successfully"})
+      
+     
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  module.exports = router;
