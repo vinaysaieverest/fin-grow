ff31c6ed4c8ffe9fc9d3c8a3ffa41f99587e95d6
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-07 21:33:08 +0530

    Implements the backend routes for login

---
+++ routes/post/login.ts
@@ -0,0 +1,16 @@
+import { Request, Response, Router } from 'express';
+import { DataModel } from '../../models';
+import { Login } from '../../src/main/post/login';
+const express = require("express")
+const router = express.Router();
+router.post('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { name,password} = req.body as { name:string,password:string};
+      const user = new Login (name , password)
+      const results = await user.setLogin()
+      res.status(200).send("Login Success")
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  module.exports = router;
