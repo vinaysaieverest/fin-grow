d254bf4d712d5ad1a6e41afc0a07f27b24ebd015
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-07 21:11:32 +0530

    Updates test cases for authentication

--- src/Test/authentication.test.ts
+++ src/Test/authentication.test.ts
@@ -1,16 +1,31 @@
-import { login } from "../main/login"
+import { Login } from "../main/post/login"
 
 describe("This is about Login",()=>{
     test('should check aboout username and password',()=>{
-        expect(login("vinay","Vinaysai02")).toBeTruthy
+        const user = new Login("vinay","Vinaysai02")
+        const results = user.setLogin()
+        expect(results).toBeTruthy
     })
-    test('should throw error for wrong credentails',()=>{
-        expect(login("visa","1234")).toBeFalsy
+    test('should check aboout username and password',()=>{
+        const user = new Login("Vinay","Vinaysai02")
+        const results = user.setLogin()
+        expect(results).toBeFalsy
     })
-    test('should give error if any of details is not enter',()=>{
-        expect(login('','1234')).toBeFalsy
+    test('should check aboout username and password',()=>{
+        const user = new Login("vinay","Vinaysai01")
+        const results = user.setLogin()
+        expect(results).toBeFalsy
     })
-    test ('should return flase if both details not enter',()=>{
-        expect(login('','')).toBeFalsy
+    test('should check aboout username and password',()=>{
+        const user = new Login("","Vinaysai02")
+        const results = user.setLogin()
+        expect(results).toBeFalsy
+    })
+    test('should check aboout username and password',()=>{
+        const user = new Login("Vinay","")
+        const results = user.setLogin()
+        expect(results).toBeFalsy
     })
 })
+
+
