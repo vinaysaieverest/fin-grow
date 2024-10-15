4c59fe276cb6c01222f434049857b051063d916f
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-15 17:19:36 +0530

    Updates the development code for transactions

---
+++ src/Main/post/transaction.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { DataModel } from '../../../models';
+export class TransactionService {
+  name: string;
+  transactionName:string;
+  amount: number;
+  type: string;
+  category :string;
+ 
+
+  constructor(name:string,transactionName:string,amount: number, type: string,category :string ) {
+    this.name = name;
+    this.transactionName = transactionName
+    this.amount = amount;
+    this.type = type;
+    this.category = category
+  }
+
+  async makeTransaction(): Promise<boolean> {
+    try {
+      const name=this.name;
+      const transactionName=this.transactionName;
+      const amount = this.amount;
+      const type = this.type;
+      const category = this.category
+      const user = await DataModel.findOne({ name });
+      if (!user) {
+        return false ;
+      }
+      if(type==="budget" || type==="Budget"){
+        const budget = user.categories.find((Tcategory: { budgetName: string }) => Tcategory.budgetName === category)
+        console.log(budget)
+        if (!budget) {
+          console.log("No budget found")
+          return false;
+        }
+        
+          user.salary -= amount;
+          budget.amount -= amount;
+        
+        user.transaction.push({ transactionName, amount, type,category });
+        await user.save();
+  
+      }
+      if(type==="saving"||type==="Saving"){
+        const budget = user.saving.find((Tcategory: { goal: string }) => Tcategory.goal === category)
+        if (!budget) {
+          return false;
+        }
+        budget.currentAmount += amount;
+        user.transaction.push({ transactionName, amount, type,category});
+        await user.save();
+      }
+      return true
+    } catch (error) {
+      return false;
+    }
+  }
+}
+
