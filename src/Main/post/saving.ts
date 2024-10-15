de1c6a3761eb2f3d8d8d632f2cbc0f92d98e46ee
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-15 17:22:34 +0530

    Updates the savings development code

---
+++ src/Main/post/saving.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { DataModel } from "../../../models";
+export class Saving{
+    name:string;
+    savingName:string;
+    amount:number
+    constructor(name:string,savingName:string,amount:number){
+        this.name=name;
+        this.savingName=savingName
+        this.amount=amount
+    }
+    async setSaving() {
+        try{
+            const name = this.name;
+            const savingName = this.savingName;
+            const amount = this.amount
+            const currentAmount = 0
+            const user = await DataModel.findOne({ name });
+            if (!user) {
+              return false;
+            }
+            user.savings.push({ goal:savingName, totalSavings:amount});
+            await user.save();
+        }
+        catch(e){
+            console.log(e)
+            return false
+        }
+
+      
+    }
+}
