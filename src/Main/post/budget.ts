d97d991437a33e602319641fd771a93b270cc4bd
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-15 12:38:01 +0530

    Implements the budget development code

---
+++ src/Main/post/budget.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { DataModel } from "../../../models";
+
+export class Budget {
+    name:string;
+    category:string;
+    amount:number;
+
+    constructor(name: string, category: string,amount:number) {
+        this.name = name;
+        this.category = category;
+        this.amount=amount
+    }
+
+    async setBudget(){
+        try{
+            const name = this.name
+            const category=this.category;
+            const amount = this.amount
+            const user = await DataModel.findOne({ name });
+            if (!user) {
+              return false;
+            }
+            user.categories.push({budgetName:category,amount});
+            await user.save();
+        }
+        catch(e){
+            console.log("Network Error")
+            return false
+        }
+
+    }
+
+}
+
