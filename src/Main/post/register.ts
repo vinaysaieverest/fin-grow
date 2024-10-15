ca3d423f46ed44baaed7c96553d6532c47de1c0e
Admin <admin@Admins-MacBook-Pro-8.local>
2024-10-15 17:43:31 +0530

    Updates the register page

--- src/Main/post/register.ts
+++ src/Main/post/register.ts
@@ -1,6 +1,4 @@
-import axios from "axios";
-import { DataModel, dataSchema } from "../../../models";
-
+import { DataModel } from "../../../models";
 export class Register {
     name:string;
     password:string;
@@ -11,19 +9,25 @@ export class Register {
         this.password = password;
         this.salary=salary
     }
-
      async setRegister(){
-            try{
+            try{    
+                const user1 = await DataModel.findOne({ name:this.name });
+                if (user1) {
+                return  false;
+            }
                 const user = new DataModel ({
                     name:this.name,
                     password:this.password,
                     salary:this.salary
+                    
                 })
+                if (this.name === "" || this.password === '' || this.salary === 0) {
+                    return false; 
+                }
                 await user.save();
-                return "User Created Succesfully"
             }
             catch(e){
-                console.log(e)
+                return false
             }
             
         }
